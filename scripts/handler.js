bac05384dd9e70906255440627fa1bb0f9f1f370
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2020-02-05 19:28:44 +0100

    provide function call

--- scripts/handler.js
+++ scripts/handler.js
@@ -2,7 +2,8 @@ function calculateHandler(event) {
   // read & process user input from event
 
   // pass user input through core logic
-
+  const result = doMath(op, x, y); // leave this line!
+  
   // render output to DOM for user
 
   // log user action for developers
