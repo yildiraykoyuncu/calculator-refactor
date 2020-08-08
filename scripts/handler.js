f6ce4b008dc604662240845b7d538708a4a2304e
Yıldıray Koyuncu <61549573+yildiraykoyuncu@users.noreply.github.com>
2020-08-08 09:05:41 +0200

    added handler close #4

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,13 +1,22 @@
 function calculateHandler(event) {
-  // read & process user input from event
+    // read & process user input from event
+    let inputOne = document.getElementById('input-1').value;
+    let x = Number(inputOne);
 
-  // pass user input through core logic
-  const result = doMath(op, x, y); // leave this line!
-  
-  // render output to DOM for user
+    let inputTwo = document.getElementById('input-2').value;
+    let y = Number(inputTwo);
 
-  // log user action for developers
+    let op = document.getElementById('operator').value;
 
-  // return true for the browser
-  return true;
+    // pass user input through core logic
+    const result = doMath(op, x, y); // leave this line!
+
+    // render output to DOM for user
+
+    document.getElementById('display').innerHTML = result;
+
+    // log user action for developers
+
+    // return true for the browser
+    return true;
 }
